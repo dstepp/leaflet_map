62e2ea864a0a61e18dcb270fd09a08d1a2787a5c
David Stepp AFS <david.stepp@accenturefederal.com>
2018-04-29 19:25:50 -0400

    Added point to cluster
    Added starting point to cluster layer and updated start point on map. Changed title.

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 
-var map = L.map('map').setView([38.862, -77.05], 10);
+var map = L.map('map').setView([38.862, -77.05], 12);
 
 // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 // 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -9,7 +9,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([38.862, -77.05]).addTo(map)
+var usms = L.marker([38.862, -77.05]).addTo(map)
 		.bindPopup('United States Marshalls Service')
 		.openPopup();
 
@@ -39,5 +39,6 @@ var geojson = L.geoJSON(data, {
 
 
 var markers = L.markerClusterGroup();
+markers.addLayer(geojson).addLayers([usms]);
 markers.addLayer(geojson);
 map.addLayer(markers);
